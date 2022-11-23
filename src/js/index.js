62dff11e9195026df5430e99d93220882aad96ea
Cassiano Dias <cassiano97@live.com>
2022-11-23 16:53:32 -0300

    alinhamento texto biografia

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,12 @@
 /* 
 
-    OBJETIVO - quando  clicarmos na  aba temos que mostrar o 
-    conteúdo da aba que foi clicada e esconder o conteúdo da 
-    aba anterior.
+    OBJETIVO - quando  clicarmos na  aba temos que mostrar o conteúdo da aba que foi clicada e esconder o conteúdo da aba anterior.
 
-    - passo 1 - dar um jeito de pegar os elementos que repre-
-    sentam as abas no HTML
+    - passo 1 - dar um jeito de pegar os elementos que representam as abas no HTML
 
-    - passo 2 - dar  um jeito de identificar  o clique no ele-
-    mento da aba
+    - passo 2 - dar  um jeito de identificar  o clique no elemento da aba
 
-    - passo 3 - quando o usuário clicar, desmarcar a aba sele-
-    cionada
+    - passo 3 - quando o usuário clicar, desmarcar a aba selecionada
 
     - passo 4 - marcar a aba clicada como selecionado
 
